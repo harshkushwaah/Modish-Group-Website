7b12b18a5f0464f2b0974eabf18f55ee0c317166
Cherry <harshkingkushwah@gmail.com>
2021-09-10 02:28:55 +0530

    commit 2 10/09/2 02:29 IST 24-H

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.get('/', (req, res) => {
     res.render("home");
 });
