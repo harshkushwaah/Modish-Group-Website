46724bb460c71b99b1d4f3d27e9e6fc2c6eb42e8
Cherry <harshkingkushwah@gmail.com>
2021-09-12 20:56:40 +0530

    commit 5 12/09/2 07:32 bug fixes  24-H

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
 app.get('/', (req, res) => {
     res.render("home");
 });
@@ -18,4 +19,4 @@ app.get('/', (req, res) => {
 
 // listen functions
 const port = process.env.PORT || 5000
-app.listen(port , ()=> console.log(' Server is up and running on port : ' + port));
+app.listen(port, ()=> console.log(' Server is up and running on port : ' + port));
