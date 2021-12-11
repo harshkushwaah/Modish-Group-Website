0987f9fb9996319871cf2456f212fb64dcbeb4f1
Cherry <harshkingkushwah@gmail.com>
2021-12-11 20:15:32 +0530

    message mongo db added

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+//Set up default mongoose connection
+const mongoDB = 'mongodb+srv://Admin:123@cluster0.9femo.mongodb.net/messagess';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true}).then(() => {
+    console.log("DB Connection success");
+  }).catch((err) => {
+    console.log(err);
+  });
+//schema
+const noteSchema = mongoose.Schema({
+  
+    message:{
+        type: String,
+       
+    }
+
+})
+const Message = new mongoose.model("Message", noteSchema);
 
 
 const app = express();
@@ -13,8 +32,20 @@ app.get('/', (req, res) => {
 
 app.get('/about-us', (req, res) => {
     res.render("about");
+   
 });
 
+app.post('/', (req, res) => {
+    let newNote = new Message({
+        message : req.body.messages,
+    });
+    newNote.save().then(item =>{
+        console.log("Data saved");
+     });
+     
+     res.redirect('/');
+     
+});
 
 
 
